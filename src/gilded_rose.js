498d0572fee9af8192da797fab75a0b8646dfe1c
Denys Mikhov <denys_mikhov@epam.com>
2019-11-11 19:21:44 +0200

    aged  brie quality

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -2,9 +2,10 @@ class Item {
     constructor(name, sellIn, quality){
         this.name = name;
         this.sellIn = sellIn;
-        this.quality = quality;
+        this.quality = quality && typeof quality === 'number' && quality > 50 ? 50 : quality;
     }
 }
+
 
 class Shop {
     constructor(items=[]){
@@ -13,101 +14,94 @@ class Shop {
         this.table = undefined;
     }
 
-    updateItem (item, isIncreased=true,fieldToUpdate='quality' ) {
-        if (isIncreased) {
-            item[fieldToUpdate]+=1
+
+    updateItem(item, isIncrease = true, fieldToUpdate = 'quality') {
+        let quantity = 1
+
+
+        if (isIncrease) {
+            item[fieldToUpdate] += quantity
         } else {
-            item[fieldToUpdate]-=1
+            item[fieldToUpdate] -= quantity
         }
-        if (item[fieldToUpdate]<0) {
-            item[fieldToUpdate]=0
+
+
+        if(item[fieldToUpdate] < 0 && fieldToUpdate === 'quality') {
+            item[fieldToUpdate] = 0
         }
+
+
         if (this.table) {
-        let updateQuery = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
-        let query = this.dbConnection.format(updateQuery,
-            [this.table, "quality", this.items[i].quality, "name", this.items[i].name]);
+            let updateQuery = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+            let query = this.dbConnection.format(updateQuery,
+                [this.table, "quality", item.quality, "name", item.name]);
 
-        this.dbConnection.query(query,(err, response) => {
-        if(err) {
-            console.error(err);
+
+            this.dbConnection.query(query,(err, response) => {
+                if(err) {
+                    console.error(err);
+                }
+                console.log(response.affectedRows);
+            });
+        } else {
+            console.log("Fatal error: no table name!");
         }
-        console.log(response.affectedRows);
-    });
-
-    } else {
-        console.log("Fatal error: no table name!");
     }
-}
+
 
     updateQuality() {
         for (var i = 0; i < this.items.length; i++) {
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if (this.items[i].quality > 0) {
                     if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-
-
                         this.updateItem(this.items[i], false);
                     }
                 }
             } else {
                 if (this.items[i].quality < 50) {
-
                     this.updateItem(this.items[i]);
+
 
                     if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
                         if (this.items[i].sellIn < 11) {
                             if (this.items[i].quality < 50) {
-
                                 this.updateItem(this.items[i]);
-
                             }
                         }
                         if (this.items[i].sellIn < 6) {
                             if (this.items[i].quality < 50) {
-
-
                                 this.updateItem(this.items[i]);
-
                             }
                         }
                     }
                 }
             }
             if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-
-                this.updateItem(this.items[i],false,'sellIn')
-
+                this.updateItem(this.items[i], false, 'sellIn');
             }
             if (this.items[i].sellIn < 0) {
                 if (this.items[i].name != 'Aged Brie') {
                     if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                         if (this.items[i].quality > 0) {
                             if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-
                                 this.updateItem(this.items[i], false);
-
                             }
                         }
                     } else {
-                        this.items[i].quality = 1;
-
+                        this.items[i].quality = 0;
                         this.updateItem(this.items[i], false);
-
                     }
                 } else {
                     if (this.items[i].quality < 50) {
-
                         this.updateItem(this.items[i]);
-
                     }
                 }
             }
         }
 
+
         return this.items;
     }
-
-
 }
 module.exports = {
     Item,
--- test/test_gilded_rose.js
+++ test/test_gilded_rose.js
@@ -27,6 +27,22 @@ describe("Gilded Rose", function() {
       expect(item.quality).to.equal(0);
     })
   })
+
+  it('Aged Brie quality should be increased', function() {
+    const gildedRose = new Shop([ new Item("Aged Brie", 1, 0) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(1);
+  })
+
+  it('Item quality should be less or equal 50', function() {
+    const gildedRose = new Shop([ new Item("Aged Brie", 1, 49), new Item("Item", 50, 90) ]);
+    gildedRose.updateQuality();
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(50);
+    expect(items[1].quality).to.equal(48);
+  })
+
+
 });
 
 
